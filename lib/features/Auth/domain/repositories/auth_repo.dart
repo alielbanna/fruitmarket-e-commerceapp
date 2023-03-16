b242d167fbff61fa88d7bbb012592d3cf4cc198d
Administrator <alielbana6662@gmail.com>
2023-03-16 02:39:27 +0200

    update

---
+++ lib/features/Auth/domain/repositories/auth_repo.dart
@@ -0,0 +1,15 @@
+import 'package:dartz/dartz.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:fruitmarket/features/Auth/domain/models/user_model.dart';
+
+abstract class AuthRepo {
+  Future<Either<Exception, UserModel>> loginWithGoogle();
+  Future<Either<Exception, UserModel>> loginWithFacebook();
+  Future<Either<Exception, UserModel>> completeInformation({
+    @required String name,
+    @required String phoneNumber,
+    @required String address,
+    @required UserModel user,
+  });
+}
